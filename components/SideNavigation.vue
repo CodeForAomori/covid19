d7a33f0e3351acf9721c535d67c1c581374f55f3
kaibashira hodade <hodade@gmail.com>
2020-03-25 19:14:04 +0900

    フッター表記の修正

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -92,7 +92,7 @@
           </a>
           {{ $t('の下に提供されています。') }}
           <br />
-          2020 Tokyo Metropolitan Government
+          2020 Code for Aomori
         </small>
       </footer>
     </div>
