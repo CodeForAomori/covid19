dad3f9efa9e8b66044421fd042108ee8af9ff190
gcp-web-m <root@gcp-web-m>
2020-03-25 14:54:10 +0900

    陽性患者数棒グラフを当日のデータも表示するように修正

--- utils/formatGraph.ts
+++ utils/formatGraph.ts
@@ -13,13 +13,14 @@ export type GraphDataType = {
  * Format for *Chart component
  *
  * @param data - Raw data
+ * @param is_date_filter - enabled date filter
  */
-export default (data: DataType[]) => {
+export default (data: DataType[], is_date_filter: Boolean = true) => {
   const graphData: GraphDataType[] = []
   const today = new Date()
   let patSum = 0
   data
-    .filter(d => new Date(d['日付']) < today)
+    .filter(d => (!is_date_filter || new Date(d['日付']) < today))
     .forEach(d => {
       const date = new Date(d['日付'])
       const subTotal = d['小計']
