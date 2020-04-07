33067fc6be89665b4f1ea4e5fec0d7f71dde5ba4
gcp-web-m <root@gcp-web-m>
2020-04-07 09:05:36 +0900

    陽性患者確認数のデータを自動取得するJSONのデータを使用するように修正

--- components/cards/ConfirmedCasesNumberCard.vue
+++ components/cards/ConfirmedCasesNumberCard.vue
@@ -5,7 +5,7 @@
       :title-id="'number-of-confirmed-cases'"
       :chart-id="'time-bar-chart-patients'"
       :chart-data="patientsGraph"
-      :date="Data.patients.date"
+      :date="date"
       :unit="$t('人')"
       :url="'https://opendata.pref.aomori.lg.jp/dataset/1531.html'"
       :url-text="'出典元：青い森オープンデータカタログ'"
@@ -14,9 +14,11 @@
 </template>
 
 <script>
-import Data from '@/data/data.json'
+import Inspection from '@/data/inspection.json'
+import InspectionDataset from '@/data/_inspection.json'
 import formatGraph from '@/utils/formatGraph'
 import TimeBarChart from '@/components/TimeBarChart.vue'
+import dayjs from 'dayjs'
 
 export default {
   components: {
@@ -24,10 +26,15 @@ export default {
   },
   data() {
     // 感染者数グラフ
-    const patientsGraph = formatGraph(Data.patients_summary.data, false)
+    const patientsGraph = formatGraph(InspectionDataset.filter(v => v['検査日時']).map(v => ({
+      '日付': dayjs(v['検査日時'].replace(/[年月]/g, '/').replace(/日/g, '')).format('YYYY-MM-DDTHH:mm:ss.000z'),
+      '小計': Number(v['陽性数'])
+    })), false)
+    const date = Inspection.date
+    console.log()
 
     const data = {
-      Data,
+      date,
       patientsGraph
     }
     return data
